aa7b75b33ef376a7127813c3055dd263f2ba129e
Linlin Cui <linlin.cui@noaa.gov>
2025-08-19 13:20:46 +0000

    rename file and job function

--- oper/wcoss2/submit_mlgefs_job_wcoss2.py
+++ oper/wcoss2/submit_mlgefs_job_wcoss2.py
@@ -45,7 +45,7 @@ def get_job_id(command):
     return job_id
 
 
-def submit_slurm_run(member, param, model_id, curr_datetime, prev_datetime):
+def submit_job_wcoss2(member, param, model_id, curr_datetime, prev_datetime):
     ymd=curr_datetime[:8]
     cyc=curr_datetime[8:]
 
@@ -137,4 +137,4 @@ if __name__ == '__main__':
             member = f'p{int(key):02d}'
 
         param = f'{param_path}/{values.get("params")}'
-        submit_slurm_run(member, param, key, curr_datetime.strftime("%Y%m%d%H"), prev_datetime.strftime("%Y%m%d%H"))
+        submit_job_wcoss2(member, param, key, curr_datetime.strftime("%Y%m%d%H"), prev_datetime.strftime("%Y%m%d%H"))
