dccccc594299326da81a129f075ad7c529ed4269
Linlin Cui <linlin.cui@noaa.gov>
2025-08-19 12:44:22 +0000

    update syndat path

--- oper/wcoss2/cronjob_wcoss2.sh
+++ oper/wcoss2/cronjob_wcoss2.sh
@@ -1,16 +1,15 @@
 #!/bin/bash --login
 
-cd /lfs/h2/emc/nems/noscrub/linlin.cui/Tests/eagle_ensemble
-
 module load intel/19.1.3.304 
 module use /apps/dev/lmodules/intel/19.1.3.304
 module load libjpeg/9c
 module load ve/eagle/1.0
-module load awscli/2.7.35
+
+HOMEDIR=${1:-/lfs/h2/emc/nems/noscrub/$USER/mlglobal}
+JOBDIR=${HOMEDIR}/oper/wcoss2
+cd $JOBDIR
 
 # delete previous files
 rm *.out *.err *.pbs
-
-aws s3 --profile gcgfs sync s3://noaa-nws-graphcastgfs-pds/hurricanes/syndat tracker/syndat --no-sign-request
 
 python submit_mlgefs_job.py
